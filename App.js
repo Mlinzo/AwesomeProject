5e0309854f99175072fe1c6890983c46344694c1
Mlinzo <46653182+Mlinzo@users.noreply.github.com>
2022-04-14 22:25:29 +0300

    Init

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+const currentTime = ()=>{
+  const today = new Date();
+  const hours = today.getHours();
+  const minutes = today.getMinutes();
+  const seconds = today.getSeconds();
+  const time = 
+    (hours < 10 ? "0"+hours : hours) + 
+    ":" + (minutes < 10 ? "0"+minutes : minutes) + 
+    ":" + (seconds < 10 ? "0"+seconds : seconds);
+  return time;
+}
+
+const randomColor = () => {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return '#' + n.slice(0, 6);
+};
 
 export default function App() {
+  const [time, setTime] = useState(currentTime())
+  const [color, setColor] = useState(randomColor())
+
+  useEffect(() => {
+    const interval = setInterval(() => {setTime(currentTime()); setColor(randomColor()); }, 1000);
+    return () => { clearInterval(interval); };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: color, fontSize: 40}}>
+        {time}
+      </Text>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +43,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
